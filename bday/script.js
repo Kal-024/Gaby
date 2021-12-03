ca03f810b945da9b45d1583bcf40762da0545491
Harvey Vilchez <80428143+Kal-024@users.noreply.github.com>
2021-12-02 19:42:51 -0600

    Add files via upload

---
+++ bday/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "ana" && b=="02/12/1996")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
